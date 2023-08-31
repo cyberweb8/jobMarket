f9c38c3d06a25ade479396864e323bd3507a5483
cyberweb8 <abdumalikshareef@gmail.com>
2023-08-31 11:59:35 +0500

    feat: implement data retrieval from api

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-const citySelect = document.querySelector('#city');
-const choices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const API_URL = 'https://workspace-methed.vercel.app';
+const LOCATION_URL = '/api/locations';
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector('#city');
+
+  const choices = new Choices(citySelect, {
+    itemSelectText: '',
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (data) => {
+      const locations = data.map((location) => ({
+        value: location,
+      }));
+      choices.setChoices(locations, 'value', 'label', true);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+
+init();
